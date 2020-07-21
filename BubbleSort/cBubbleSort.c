a364c64e8aca4f10b67f4be3cf9b4f89babdac2f
Harris Ransom <hransom528@gmail.com>
2020-07-20 23:23:59 -0400

    Made cBubbleSort scale to large data sets

--- BubbleSort/cBubbleSort.c
+++ BubbleSort/cBubbleSort.c
@@ -2,31 +2,41 @@
 // Harris Ransom
 
 /** Includes **/
+#include <stdbool.h>
 #include <stdio.h>
 #include <string.h>
 
 /** Functions **/
 int* bubbleSort(int arr[], int len) {
 	int temp;
-	
-	for (int i = 0; i < len - 1; i++) {
-		if (arr[i] > arr[i + 1]) {
-			temp = arr[i];
-			arr[i] = arr[i + 1];
-			arr[i + 1] = temp;
+	bool swapped;
+	do {
+		swapped = false;
+		for (int i = 0; i < len - 1; i++) {
+			if (arr[i] > arr[i + 1]) {
+				temp = arr[i];
+				arr[i] = arr[i + 1];
+				arr[i + 1] = temp;
+				swapped = true;
+			}
 		}
-	}
+	} while (swapped); // Continue sorting until no swaps made
 	return arr;
 }
 
 /** MAIN **/
 int main() {
 	// Initialization
-	int nums[] = {2, 1, 3, 4, 5};
+	int nums[] = {14, 2, 20, 18, 8, 15, 1, 10, 6, 17, 4, 13, 9, 19, 3, 12, 7, 16, 5, 11};
 	int len = (sizeof(nums) / sizeof(int));
 	printf("Initial array: \n");
 	for (int i = 0; i < len; i++) {
-		printf("\tIndex %d - %d\n", i, nums[i]);
+		if (i <= 9) {
+			printf("\tIndex %d  - %d\n", i, nums[i]);
+		}
+		else {
+			printf("\tIndex %d - %d\n", i, nums[i]);
+		}
 	}
 	
 	// Sorts array
@@ -35,7 +45,12 @@ int main() {
 	// Displays results
 	printf("Bubble Sort results: \n");
 	for (int i = 0; i < len; i++) {
-		printf("\tIndex %d - %d\n", i, sorted[i]);
+		if (i <= 9) {
+			printf("\tIndex %d  - %d\n", i, sorted[i]);
+		}
+		else {
+			printf("\tIndex %d - %d\n", i, sorted[i]);
+		}
 	}
 	
 	return 0;
